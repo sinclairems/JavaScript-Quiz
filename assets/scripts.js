c71635cb96f470b02bf6fdfc9363116d51cd9b9c
Erin <50000677+sinclairems@users.noreply.github.com>
2024-01-22 14:47:26 -0600

    Initial commit, adding boilerplate html, css, and js files. README currently contains instructions for creating quiz.

---
+++ assets/scripts.js
@@ -0,0 +1,5 @@
+// incorporate timer - either total time or time/question
+// incorporate score
+// incorrect answers decrease time
+// quiz ends when all questions answered or timer runs out
+// save initials and score
